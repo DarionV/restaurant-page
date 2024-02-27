147c554f1be15bafb988a36575f7035f8370ca9e
Darion Valdez <Darionvldz@gmail.com>
2024-02-27 21:00:00 +0100

    split renderHome function to separate module

--- src/index.js
+++ src/index.js
@@ -1,30 +1,4 @@
 import "./style.css";
-
-const renderHome = function () {
-  const contentContainer = document.querySelector("#content");
-
-  const heroContainer = document.createElement("div");
-  heroContainer.classList.add("hero");
-
-  const promoText = document.createElement("img");
-  promoText.src = "/src/images/promo.svg";
-  promoText.height = 150;
-
-  const promoImage = document.createElement("img");
-  promoImage.src = "/src/images/shake_01.png";
-  promoImage.height = 400;
-  promoImage.classList.add("rounded");
-
-  heroContainer.appendChild(promoText);
-  heroContainer.appendChild(promoImage);
-
-  const promoContainer = document.createElement("div");
-  promoContainer.classList.add("promo-container");
-  const offerText = `<p>Order 1, get 5 free! Only today! (and every other day!!!)</p>`;
-  promoContainer.innerHTML = offerText;
-
-  contentContainer.appendChild(heroContainer);
-  contentContainer.appendChild(promoContainer);
-};
+import { renderHome } from "./home";
 
 renderHome();
