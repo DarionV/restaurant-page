0fae22215d6981f3d2ac725ce3590d394989dad8
Darion Valdez <Darionvldz@gmail.com>
2024-02-27 21:15:44 +0100

    add clear content function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,22 @@ import "./style.css";
 import { renderHome } from "./home";
 
 renderHome();
+
+const homeButton = document.querySelector(".js-home-button");
+const menuButton = document.querySelector(".js-menu-button");
+const aboutButton = document.querySelector(".js-about-button");
+
+homeButton.addEventListener("click", () => {
+  clearContent();
+  renderHome();
+});
+menuButton.addEventListener("click", () => {
+  clearContent();
+});
+aboutButton.addEventListener("click", () => {
+  clearContent();
+});
+
+const clearContent = function () {
+  document.querySelector("#content").innerHTML = "";
+};
